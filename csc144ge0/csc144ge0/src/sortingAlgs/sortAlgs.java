7a076511a348bbb08b210c51c4076533bc09b633
Jack Castiglione <62661858+BreadstixGIT@users.noreply.github.com>
2020-03-26 18:58:43 -0500

    Add files via upload

---
+++ csc144ge0/csc144ge0/src/sortingAlgs/sortAlgs.java
@@ -0,0 +1,129 @@
+package sortingAlgs;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+/** This program has methods to generate a random list and sort it using two basic sorting algorithms,
+ *  selection sort and insertion sort. 
+ * 
+ * @author Jack Castiglione
+ * @version 3/26/2020
+ */
+
+public class sortAlgs {
+	
+	private static final Random RNG = new Random();
+	private static final int Maximum = 100;
+	
+	/** Creates a random list of numbers with a specified length.
+	 * 
+	 * @param length The length of the generated list
+	 * @return A list of random numbers with a length equal to the length parameter
+	 */
+	public static List<Integer> makeList(Integer length){
+		List<Integer> result = new ArrayList<Integer>();
+		for (int i = 0; i< length; i++ ) {
+			int next = RNG.nextInt(Maximum);
+			result.add(next);
+		}
+		return result;
+	}
+	
+	/** Copies a list into a separate specified list location
+	 * 
+	 * @param initList The initial list to be copied
+	 * @param finalList The destination for the initial list to be copied in to
+	 *  
+	 */
+	public static void copyList(List<Integer> initList, List<Integer> finalList){
+		for (int n : initList) {
+			finalList.add(n);
+		}
+	}
+	
+	/** Finds the minimum of a list of integers
+	 * 
+	 * @param values The list of integers of which the method will find the minimum
+	 * @return The minimum value within the list
+	 */
+	public static Integer findMin(List<Integer> values) {
+		int minVal = values.get(0);
+		for (int n : values) {
+			if (n < minVal) {
+				minVal = n;
+			}
+		}
+		return minVal;
+	}
+	
+	/** Finds the position of the minimum of a list of integers
+	 * 
+	 * @param values The list of integers of which the method will find the minimum
+	 * @return The position of the first occurrence of the minimum value within the list
+	 */
+	public static Integer findPosMin(List<Integer> values) {
+		int minPos = 0;
+		int currentPos = 0;
+		int minVal = findMin(values);
+		boolean isFound = false;
+		while (currentPos < values.size() &&  !isFound) {
+			if (values.get(currentPos) == minVal) {
+				minPos = currentPos;
+				isFound = true;
+			}
+			currentPos++;
+		}
+		return minPos;
+	}
+	
+	/** Sorts a list of integers using selection sort
+	 * 
+	 * @param values The list of integers to be sorted
+	 * @return A sorted list of integers
+	 */
+	public static List<Integer> selecSort(List<Integer> values){
+		List<Integer> funcValues = new ArrayList<Integer>();
+		copyList(values, funcValues);
+		List<Integer> result = new ArrayList<Integer>();
+		int cPosMin;
+		int cMinVal;
+		while (funcValues.size() > 0) {
+			cMinVal = findMin(funcValues);
+			cPosMin = findPosMin(funcValues);
+			result.add(cMinVal);
+			funcValues.remove(cPosMin);
+		}
+		return result;
+	}
+	
+	/** Sorts a list of integers using insertion sort
+	 * 
+	 * @param values The list of integers to be sorted
+	 * @return A sorted list of integers
+	 */
+	public static List<Integer> insertSort(List<Integer> values){
+		List<Integer> funcValues = new ArrayList<Integer>();
+		copyList(values, funcValues);
+		funcValues.remove(0);
+		List<Integer> result = new ArrayList<Integer>();
+		result.add(values.get(0));
+		for (int n : funcValues) {
+			boolean isFound = false;
+			for (int m = 0; m < result.size(); m++) {
+				if (n < result.get(m) && ! isFound) {
+					result.add(m, n);
+					isFound = true;
+				}
+			}
+			if (! isFound) {
+				result.add(n);
+			}
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
